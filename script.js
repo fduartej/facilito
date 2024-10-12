13cefaabadae9ec59c708a31d1a736536ee48128
Fred Duarte <fduartej@gmail.com>
2024-10-12 10:12:20 -0500

    Agregando mi componente de navegacion

--- script.js
+++ script.js
@@ -2,6 +2,27 @@ const Welcome = () => {
   return <h1>Hello World</h1>;
 };
 
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">All pies</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
 const Formulario = () => {
   return (
     <form>
