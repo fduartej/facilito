e0d23ca41bf66ab1c677583e1b1c333baf7af67d
Fred Duarte <fduartej@gmail.com>
2024-10-19 09:58:58 -0500

    Card

--- script.card.js
+++ script.card.js
@@ -1,6 +1,6 @@
-const Card = (categoria, srcimagen, title, description, precio) => {
+const Card = (category, srcimagen, title, description, precio) => {
   return (
-    <div class="pieRow" data-category={categoria}>
+    <div class="pieRow" data-category={category}>
       <img src={srcimagen} width="100" height="63" class="pie-img" />
       <div>
         <h5 class="pie-title">{title}</h5>
@@ -10,3 +10,19 @@ const Card = (categoria, srcimagen, title, description, precio) => {
     </div>
   );
 };
+
+const CardList = ({ data }) => (
+  <div class="content-section">
+    {data.map((card, index) => (
+      <Card
+        category={card.category}
+        srcimagen={card.src}
+        title={card.title}
+        description={card.description}
+        price={card.price}
+      />
+    ))}
+  </div>
+);
+
+export default CardList;
