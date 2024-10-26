a2a50b80a524710f9ffa1d35020955497879fd0a
Fred Duarte <fduartej@gmail.com>
2024-10-26 08:04:33 -0500

    Agregando Tailwind css

--- component.formulario.js
+++ component.formulario.js
@@ -1,13 +1,46 @@
 const Formulario = () => {
   return (
-    <form class="contact-form">
-      <label for="name">Your Name:</label>
-      <input type="text" id="name" placeholder="enter your name" />
-      <label for="email">Your Email:</label>
-      <input type="text" id="email" placeholder="enter your name" />
-      <label for="message">Your Message:</label>
-      <textarea id="message" placeholder="enter your message"></textarea>
-      <input type="button" value="Send" />
+    <form className="p-6 bg-white shadow-md rounded-lg space-y-4">
+      <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+        Your Name:
+      </label>
+      <input
+        type="text"
+        id="name"
+        placeholder="Enter your name"
+        className="w-full p-2 border border-gray-300 rounded"
+      />
+
+      <label
+        htmlFor="email"
+        className="block text-sm font-medium text-gray-700"
+      >
+        Your Email:
+      </label>
+      <input
+        type="text"
+        id="email"
+        placeholder="Enter your email"
+        className="w-full p-2 border border-gray-300 rounded"
+      />
+
+      <label
+        htmlFor="message"
+        className="block text-sm font-medium text-gray-700"
+      >
+        Your Message:
+      </label>
+      <textarea
+        id="message"
+        placeholder="Enter your message"
+        className="w-full p-2 border border-gray-300 rounded"
+      ></textarea>
+
+      <input
+        type="button"
+        value="Send"
+        className="w-full py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+      />
     </form>
   );
 };
